0095863e2982614e0ed3cc6b29431455caabb9d0
Jardel-Nathan <jardelnathan22@hotmail.com>
2017-11-26 13:28:04 -0200

    login

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,13 +17,9 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
-          echo "entrou";
-          exit;
-            return redirect('/home');
-        }
-echo 'nao entrou';
-exit;
+
+            return redirect('admin/home');
+
         return $next($request);
     }
 }
