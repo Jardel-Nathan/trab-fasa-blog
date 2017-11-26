7728ca08ab2d935b98e501a000961389725eb21d
Jardel-Nathan <jardelnathan22@hotmail.com>
2017-11-26 11:46:25 -0200

    committested

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ env('URL_ADMIN_LOGIN') }}">
                         {{ csrf_field() }}
-
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail</label>
 
