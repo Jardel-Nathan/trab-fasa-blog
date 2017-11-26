4a1dca91e987bcdad5e5c81fbb104ea4caac4b51
Jardel-Nathan <jardelnathan22@hotmail.com>
2017-11-26 15:17:29 -0200

    login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -43,6 +43,7 @@ class LoginController extends Controller
     {
         $data = $request->only($this->username(), 'password');
         $data['role']=User::ROLE_ADMIN;
+        
         return $data;
     }
 
