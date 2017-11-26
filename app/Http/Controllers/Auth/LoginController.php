3dbff77b954796eb07cc60229441f0e7f168ac2e
Jardel-Nathan <jardelnathan22@hotmail.com>
2017-11-26 15:36:15 -0200

    login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -42,13 +42,13 @@ class LoginController extends Controller
     protected function credentials(Request $request)
     {
         $data = $request->only($this->username(), 'password');
-
-        $data['role']=User::ROLE_ADMIN;
-
         echo "<pre>";
         var_dump($request->input());
         var_dump($data);
         exit;
+        $data['role']=User::ROLE_ADMIN;
+
+
         return $data;
     }
 
